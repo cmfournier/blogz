943272a4e4698acd56c115dabecad7d0073e501a
Carlos Fournier <cmfournier@gmail.com>
2019-09-29 12:43:50 -0400

    Added Use Case checks and new template for ind blog entries

--- main.py
+++ main.py
@@ -16,12 +16,16 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route('/blog', methods =['POST','GET'])
+@app.route('/blog')
 def index():
+    post_id = request.args.get('id')
+    if (post_id):
+        ind_entry = Blog.query.get(post_id)
+        return render_template('ind_entry_page.html', ind_entry=ind_entry)
+    else:
+        all_posts = Blog.query.all()
+        return render_template('blog.html', title="Build A Blog", blog_posts=all_posts)
 
-
-    blog_posts = Blog.query.all()
-    return render_template('blog.html', title="Build A Blog", blog_posts=blog_posts)
         
 @app.route('/newpost', methods = ['POST','GET'])
 def newposts():
@@ -32,24 +36,31 @@ def newposts():
         title_error = ''
         post_error = ''
 
-        if len(title_name) == 0:
+        if len(title_name) == 0 and len(blog_post) == 0:
             title_error = 'You must have a title!'
-    
-        if len(blog_post) == 0:
             post_error = 'You must enter text for your blog post!'
+            
+        if len(title_name) > 0 and len(blog_post) == 0:
+            title_error = "You must have a title!"
+            return render_template('newpost.html',title_name=title_name, title_error=title_error)
         
+        if len(blog_post) > 0 and len(title_name) == 0:
+            post_error = "You must enter text for your blog post!"
+            return render_template('newpost.html', blog_post=blog_post, post_error=post_error)
+            
         if not title_error and not post_error:
 
             new_post = Blog(title_name, blog_post)
             db.session.add(new_post)
             db.session.commit()
-            return redirect('/blog')
+            return redirect('/blog?id=' + str(new_post.id))
         else:
             return render_template('newpost.html', title="Add Blog Entry", 
             title_name=title_name,title_error=title_error, 
             blog_post=blog_post, post_error=post_error)
     else:
         return render_template('newpost.html', title="Add Blog Entry")
+
 
 
 if __name__ == '__main__':
